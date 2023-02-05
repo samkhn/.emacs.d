0a94161a372d2a50262ebb33c3a724cf2fa81804
Samiur Khan <samiurkh1n@gmail.com>
2023-02-05 12:14:30 -0500

    Adds LLVM and LLVM::TableGen mode for emacs

--- init.el
+++ init.el
@@ -127,7 +127,41 @@
 ;;  (add-to-list 'auto-mode-alist '("\\.ext\\'" . c-mode))
 ;; TODO: find related files (*.cpp -> *.h -> *test.cpp)
 
-;; TODO: LLVM mode
+;; NOTE: LLVM sub-config. Maintainer: LLVM Team, http://llvm.org/
+;; NOTE: If you notice missing or incorrect syntax highlighting, please contact
+;;   <llvm-bugs [at] lists.llvm.org>
+(add-to-list 'load-path "~/.emacs.d/pkgs/llvm-mode/")
+(defun llvm-lineup-statement (langelem)
+  (let ((in-assign (c-lineup-assignments langelem)))
+    (if (not in-assign)
+        '++
+      (aset in-assign 0
+            (+ (aref in-assign 0)
+               (* 2 c-basic-offset)))
+      in-assign)))
+
+(c-add-style "llvm.org"
+             '("gnu"
+	       (fill-column . 80)
+	       (c++-indent-level . 2)
+	       (c-basic-offset . 2)
+	       (indent-tabs-mode . nil)
+	       (c-offsets-alist . ((arglist-intro . ++)
+				   (innamespace . 0)
+				   (member-init-intro . ++)
+				   (statement-cont . llvm-lineup-statement)))))
+
+;; Files with "llvm" in their names will automatically be set to the
+;; llvm.org coding style.
+(add-hook 'c-mode-common-hook
+	  (function
+	   (lambda nil 
+	     (if (string-match "llvm" buffer-file-name)
+		 (progn
+		   (c-set-style "llvm.org"))))))
+
+(require 'llvm-mode)
+(require 'tablgen-mode)
 
 (add-to-list 'load-path "~/.emacs.d/pkgs/rust-mode/")
 (autoload 'rust-mode "rust-mode" nil t)
